048e08b04ddad5e1223c3d82cde072f7a8878271
Jay Millena <jay.millena07@gmail.com>
2024-08-22 19:21:45 +0800

    Added comment

--- comment.php
+++ comment.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect("localhost", "root", "", "");
+    $conn = mysqli_connect("localhost", "root", "", "myfirstphp");
 
     $name = $_POST['name'];
     $comment = $_POST['comment'];
@@ -9,5 +9,28 @@
     if($send)
     {
         echo "$name: $comment";
+        $insert_query = "INSERT INTO `comments` 
+                        (
+                            `name`, 
+                            `comment`
+                        ) 
+                        VALUES 
+                        (
+                            '$name', 
+                            '$comment'
+                        )";
+
+        $select_db = mysqli_select($conn, 'myfirstphp');
+        $save = mysqli_query($conn, $insert_query);
+
+        if($save)
+        {
+            echo "Comment has been saved";    
+        }
+        else 
+        {
+            echo "Comment has not been saved";
+        }
+        
     }
 ?>
